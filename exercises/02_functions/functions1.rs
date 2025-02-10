d5c2460d0bc3d78a4d9cd7324350a70cc0396669
Rashid <rasid.uae2002@hotmail.com>
2025-02-10 10:07:27 +0400

    Lab3 finished funcrions 1:4, if 1:2, options 1:2

--- exercises/02_functions/functions1.rs
+++ exercises/02_functions/functions1.rs
@@ -2,4 +2,12 @@
 
 fn main() {
     call_me(); // Don't change this line
+    
 }
+// TODO: Add some function with the name `call_me` without arguments or a return value.
+
+fn call_me() {
+    println!("wow I am Rash");
+}
+
+
--- exercises/03_if/if2.rs
+++ exercises/03_if/if2.rs
@@ -2,8 +2,14 @@
 fn picky_eater(food: &str) -> &str {
     if food == "strawberry" {
         "Yummy!"
-    } else {
-        1
+    } else if food == "potato" {
+        "I guess I can eat that."
+    }else if food == "broccoli" {
+        "No thanks!"
+    }else if food == "gummy bears" {
+        "No thanks!"
+    }else  {
+        "No thanks!"
     }
 }
 
